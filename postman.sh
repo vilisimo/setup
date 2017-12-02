1ff29e5086e50241e77c4de9d828cec3fdbcb9da
Simonas Viliunas <vilisimo@gmail.com>
2017-12-02 20:10:31 +0200

    Add installation script for postman

---
+++ postman.sh
@@ -0,0 +1,19 @@
+# Download and extract x64 Postman
+curl -o ~/Downloads/postman.tar.gz https://dl.pstmn.io/download/latest/linux64
+sudo tar -xf ~/Downloads/postman.tar.gz -C /opt
+rm ~/Downloads/postman.tar.gz
+
+# Create soft link to launcher script so that it's accessible from cli
+sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+# Point entry to Postman icon
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/resources/app/assets/icon.png
+Terminal=false
+Categories=Development;
+EOL
+
