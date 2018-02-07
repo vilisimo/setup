9636782b7e3d2e6986bf81d572918a170df53ad7
Simonas Viliunas <vilisimo@gmail.com>
2018-02-07 09:27:28 +0200

    Add git settings setup

---
+++ settings/git/setup-git.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+read -p "Please enter your username: " username
+read -p "Please enter your email: " email
+
+sed -i "2i\ \ \ \ \ \ \ \ \ \ name = $username" gitconfig
+sed -i "3i\ \ \ \ \ \ \ \ \ \ email = $email" gitconfig
+
+mv gitconfig ~/.gitconfig
