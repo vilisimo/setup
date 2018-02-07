694f2744ab71fa36342339dce9ec0e59b0bb7340
Simonas Viliunas <vilisimo@gmail.com>
2018-02-07 20:46:29 +0200

    Rename variable

--- settings/git/setup-git.sh
+++ settings/git/setup-git.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-read -p "Please enter your username: " username
+read -p "Please enter your user: " user
 read -p "Please enter your email: " email
 
-sed -i "s/placeholdername/$username/" gitconfig
+sed -i "s/placeholdername/$user/" gitconfig
 sed -i "s/placeholderemail/$email/" gitconfig
 
 mv gitconfig ~/.gitconfig
