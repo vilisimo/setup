be8f0095a89a42412714db8faa799a812b9795be
vilisimo <vilisimo@gmail.com>
2022-08-25 01:27:57 +0300

    Improve mac scripts

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,2 +1,21 @@
-# Setup sublime shortcut
+echo "Creating sublime shortcut: 'subl'"
 sudo ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
+echo "Changing default shell to bash: password will be required"
+chsh -s /bin/bash
+echo ""
+echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_profile
+
+read -p "Overwriting ~/.bash_profile: are you sure?
+> " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]] 
+then
+    echo "Overwriting..."
+    cp bash_profile ~/.bash_profile
+else
+    echo "Cancelling..."
+fi
+
+source ~/.bash_profile
+
